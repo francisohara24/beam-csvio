ca26a794db7957380fc9eacf47e568703da227de
francisohara24 <francisohara@google.com>
2024-07-03 16:17:27 +0000

    Feat: update class for testing how CSVFormat's withAllowDuplicateHeaderNames() parameter works.

--- src/main/java/csv_format/WithAllowDuplicateHeaderNames.java
+++ src/main/java/csv_format/WithAllowDuplicateHeaderNames.java
@@ -1,13 +1,15 @@
 package csv_format;
 
 import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVParser;
 
 import java.io.IOException;
 
 public class WithAllowDuplicateHeaderNames {
   public static void main(String[] args) throws IOException {
-    CSVFormat format = CSVFormat.DEFAULT.withAllowDuplicateHeaderNames(false).withHeader("foo", "bar");  // will run correctly
-    CSVFormat formatWithDuplicateHeaderNames = CSVFormat.DEFAULT.withAllowDuplicateHeaderNames(false).withHeader("foo", "foo");  // will throw exception
+    // will run correctly
+    CSVFormat format = CSVFormat.DEFAULT.withAllowDuplicateHeaderNames(false).withHeader("foo", "bar");
+
+    // will throw exception
+    CSVFormat formatWithDuplicateHeaderNames = CSVFormat.DEFAULT.withAllowDuplicateHeaderNames(false).withHeader("foo", "foo");
   }
 }
