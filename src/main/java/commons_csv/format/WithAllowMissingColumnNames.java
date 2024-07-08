5b655619cde7a44cfd687045de1a5ce0a1c7c5b8
francisohara24 <francisohara@google.com>
2024-07-08 06:58:11 +0000

    feat: add examples.

--- src/main/java/commons_csv/format/WithAllowMissingColumnNames.java
+++ src/main/java/commons_csv/format/WithAllowMissingColumnNames.java
@@ -1,8 +1,34 @@
 package commons_csv.format;
 
 import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+
+import java.io.IOException;
 
 public class WithAllowMissingColumnNames {
-  CSVFormat format = CSVFormat.DEFAULT.withAllowMissingColumnNames();
+  public static void main (String[] args) throws IOException {
+    // will run successfully
+    CSVFormat formatWithHeaderContainingNull_true = CSVFormat.DEFAULT.withAllowMissingColumnNames(true).withHeader("foobar", null);
+    CSVParser.parse("foo,bar", formatWithHeaderContainingNull_true);
 
+    // will run successfully
+    CSVFormat formatWithHeaderContainingEmptyString_true = CSVFormat.DEFAULT.withAllowMissingColumnNames(true).withHeader("foobar", "");
+    CSVParser.parse("foo,bar", formatWithHeaderContainingEmptyString_true);
+
+    // will throw exception
+    try {
+      CSVFormat formatWithHeaderContainingNull_false = CSVFormat.DEFAULT.withAllowMissingColumnNames(false).withHeader("foobar", null);
+      CSVParser.parse("foo,bar", formatWithHeaderContainingNull_false);
+    } catch(IllegalArgumentException e){
+      System.out.println("Exception thrown: " + e.getMessage());
+    }
+
+    // will throw exception
+    try {
+      CSVFormat formatWithHeaderContainingEmptyString_false = CSVFormat.DEFAULT.withAllowMissingColumnNames(false).withHeader("foobar", "");
+      CSVParser.parse("foo,bar", formatWithHeaderContainingEmptyString_false);
+    } catch (IllegalArgumentException e){
+      System.out.println("Exception thrown: " + e.getMessage());
+    }
+  }
 }
