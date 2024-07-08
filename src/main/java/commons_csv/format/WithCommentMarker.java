4cda744c6b39ad34ad368d602d0decc3f832e39c
francisohara24 <francisohara@google.com>
2024-07-08 16:39:08 +0000

    feat: add example

--- src/main/java/commons_csv/format/WithCommentMarker.java
+++ src/main/java/commons_csv/format/WithCommentMarker.java
@@ -1,7 +1,17 @@
 package commons_csv.format;
 
-public class WithCommentMarker {
-  public static void main(String[] args) {
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
 
+import java.io.IOException;
+
+public class WithCommentMarker {
+  public static void main(String[] args) throws IOException {
+    CSVFormat format = CSVFormat.DEFAULT;
+    CSVFormat formatWithCommentMarker = CSVFormat.DEFAULT.withCommentMarker('\n');
+    String records = "foo,bar,foobar\nfoo,bar,#foobar\n#foo,bar,foobar";  // the last line will be treated as a comment
+
+    System.out.println(CSVParser.parse(records, format).getRecords());  //  3 records
+    System.out.println(CSVParser.parse(records, formatWithCommentMarker).getRecords());  // only 2 records
   }
 }
