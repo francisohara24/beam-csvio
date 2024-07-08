f84429fce7c7d62a84327eb5e7d60939495b0f59
francisohara24 <francisohara@google.com>
2024-07-08 16:52:58 +0000

    feat: add example

--- src/main/java/commons_csv/format/WithDelimiter.java
+++ src/main/java/commons_csv/format/WithDelimiter.java
@@ -1,7 +1,17 @@
 package commons_csv.format;
 
-public class WithDelimiter {
-  public static void main(String[] args) {
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
 
+import java.io.IOException;
+
+public class WithDelimiter {
+  public static void main(String[] args) throws IOException {
+    CSVFormat format = CSVFormat.DEFAULT.withDelimiter(';');
+    String recordWithDelimiter = "foo;bar;foobar";  // record with 3 fields/cells
+    String recordWithoutDelimiter = "foo,bar,foobar";  // record with 1 field/cell
+
+    System.out.println(CSVParser.parse(recordWithDelimiter, format).getRecords());
+    System.out.println(CSVParser.parse(recordWithoutDelimiter, format).getRecords());
   }
 }
