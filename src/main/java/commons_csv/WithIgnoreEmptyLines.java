77e8dad4b31a8011322cd8584925c4198e005e8e
francisohara24 <francisohara@google.com>
2024-07-09 21:52:01 +0000

    feat: add examples.

--- src/main/java/commons_csv/WithIgnoreEmptyLines.java
+++ src/main/java/commons_csv/WithIgnoreEmptyLines.java
@@ -1,5 +1,21 @@
 package commons_csv;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+
+import java.io.IOException;
+
 public class WithIgnoreEmptyLines {
-  public static void main(String[] args) {}
+  public static void main(String[] args) throws IOException {
+    CSVFormat formatWithoutIgnoreEmptyLines = CSVFormat.newFormat(',').withIgnoreEmptyLines(false);
+    CSVFormat formatWithIgnoreEmptyLines = CSVFormat.newFormat(',').withIgnoreEmptyLines(true);
+    String recordWithEmptyLine = "foo,bar,baz\n\n1,2,3";
+
+
+    // Will result in two CSV records since empty line is ignored
+    System.out.println(CSVParser.parse(recordWithEmptyLine,formatWithIgnoreEmptyLines).getRecords());
+
+    // Will result in 3 CSV records where the middle CSV record is completely empty.
+    System.out.println(CSVParser.parse(recordWithEmptyLine, formatWithoutIgnoreEmptyLines).getRecords());
+  }
 }
