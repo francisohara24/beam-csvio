b8e7a5bb7e1c59b3aab4a5a31ab20dc5a699eb9b
francisohara24 <francisohara@google.com>
2024-07-09 17:28:29 +0000

    feat: add example

--- src/main/java/commons_csv/format/WithHeaderComments.java
+++ src/main/java/commons_csv/format/WithHeaderComments.java
@@ -1,7 +1,17 @@
 package commons_csv.format;
 
-public class WithHeaderComments {
-  public static void main(String[] args) {
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVPrinter;
 
+import java.io.IOException;
+import java.util.Arrays;
+
+public class WithHeaderComments {
+  public static void main(String[] args) throws IOException {
+    CSVFormat format = CSVFormat.DEFAULT.withHeader("column1", "column2").withCommentMarker('#').withHeaderComments("This is the first header comment.","This is the second header comment.");
+
+    // Two header comments will be printed before the header and CSV records.
+    System.out.println(format.format("foo", "bar"));
   }
 }
